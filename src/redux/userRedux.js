05416026b5fa614d4b75aeb5c747e0662ccc6fd0
M00n-man92 <azraelnumb00@gmail.com>
2022-05-15 03:16:21 +0300

    something ain't right

--- src/redux/userRedux.js
+++ src/redux/userRedux.js
@@ -14,11 +14,12 @@ const userSlice = createSlice(
             },
             loginSuccess:(state,action)=>{
                 state.isFetching=false;
-                state.currentUser=action.payload
+                state.currentUser=action.payload;
+                state.error=false;
              
             },loginError:(state)=>{
                 state.isFetching=false;
-                state.error=true
+                state.error=true;
             },
             logoutStart:(state)=>{
                 state.isFetching=true;
